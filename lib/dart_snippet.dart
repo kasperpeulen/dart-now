a0a05d62cfdaed02de36c82493b383a97757acda
Kasper Peulen <kasperpeulen@gmail.com>
2015-07-19 13:12:31 +0200

    add firebase integration

--- lib/dart_snippet.dart
+++ lib/dart_snippet.dart
@@ -1,32 +1,23 @@
 library dartnow.dart_snippet;
 
-import 'package:github/browser.dart';
-import 'package:dartnow/pub_spec.dart';
-
 class DartSnippet {
-  Gist gist;
-  String name;
-  String description = "";
-  PubSpec pubspec;
+  final String name;
+  final String description;
+  final String mainLibrary;
+  final String mainElements;
+  final String tags;
+  final String id;
+  final String author;
+  final String dartpad;
+  final String gist;
 
-  String centralLibrary;
-  Set<String> centralElements;
-
-  DartSnippet(this.gist) {
-    name = gist.description;
-    var pubspecFile =
-        gist.files.firstWhere((file) => file.name == 'pubspec.yaml');
-    pubspec = new PubSpec.fromString(pubspecFile.content);
-    name = pubspec.name;
-    if (pubspec.description != null) description = pubspec.description;
-    if (pubspec.name.contains('_')) {
-      centralLibrary = pubspec.name.substring(0, pubspec.name.indexOf('_'));
-      centralElements = pubspec.name
-          .substring(pubspec.name.indexOf('_') + 1)
-          .split('_')
-          .toSet();
-    } else {
-      centralLibrary = pubspec.name;
-    }
-  }
+  DartSnippet.fromJSON(this.id, Map json) :
+    name = json['name'],
+    description = json['description'],
+    mainLibrary = json['mainLibrary'],
+    mainElements = json['mainElements'],
+    tags = json['tags'],
+    author = json['author'],
+    dartpad = json['dartpad'],
+    gist = json['gist'];
 }
--- web/main.dart
+++ web/main.dart
@@ -1,24 +1,12 @@
 import 'dart:html';
 import 'dart:async';
 
-import 'package:github/browser.dart';
+import 'package:firebase/firebase.dart';
+
 import 'package:dartnow/dart_snippet.dart';
 import 'package:dartnow/common.dart';
 
-Set<String> gistIds = [
-  '8cefaa4f72db16e0f9ee',
-  '95e7eb260a737c0828dd',
-  'dd353c3d95b2da3dd74b',
-  '5a0e89c90c8a732aa744',
-  '8b288fc737b7069d04eb',
-  '1779cb03472912a1359e',
-  'fa2c0825619512750c86',
-  '9b71b6f5f094e658043c',
-  '5cd660b5088311c64872',
-].toSet();
-
-Set<Gist> gists;
-Set<DartSnippet> snippets;
+List<DartSnippet> snippets = [];
 
 DivElement snippetsDivElement = querySelector('#snippets');
 InputElement libraryInputElement = querySelector('#library');
@@ -26,25 +14,27 @@ InputElement elementInputElement = querySelector('#dart_element');
 InputElement keywordsInputElement = querySelector('#keywords');
 
 main() async {
-  await fetchGists();
-
-  filterGistsAndShow();
+  await fetchFirebase();
 
   libraryInputElement.onInput.listen((e) => filterGistsAndShow());
   elementInputElement.onInput.listen((e) => filterGistsAndShow());
   keywordsInputElement.onInput.listen((e) => filterGistsAndShow());
 }
 
-fetchGists() async {
-  // this authentication is very limited
-  // we should make a login system I guess
-  Authentication auth = new Authentication.anonymous();
-  //  var auth = new Authentication.basic('kasperpeulen', '****');
-  GitHub gitHub = createGitHubClient(auth: auth);
-  gists = (await Future.wait(
-          gistIds.map((String id) async => await gitHub.gists.getGist(id))))
-      .toSet();
-  snippets = gists.map((Gist gist) => new DartSnippet(gist)).toSet();
+fetchFirebase() async {
+  Firebase firebase = new Firebase('https://amber-fire-5576.firebaseio.com/');
+
+  firebase.child('gists').onValue.listen((e) {
+    DataSnapshot snapshot = e.snapshot;
+    Map json = snapshot.val();
+    snippets = [];
+    if (json == null) return;
+    json.forEach((String key, Map value) {
+      snippets.add(new DartSnippet.fromJSON(key, value));
+    });
+    filterGistsAndShow();
+  });
+
 }
 
 filterGistsAndShow() {
@@ -52,12 +42,12 @@ filterGistsAndShow() {
   snippetsDivElement.innerHtml = "";
 
   List<String> keywords = keywordsInputElement.value.split(' ');
-  List<String> elements = elementInputElement.value.split(', ');
+  List<String> elements = elementInputElement.value.split(' ');
   Set<DartSnippet> filtered =
       snippets.where((snippet) =>
-        snippet.centralLibrary.contains(libraryInputElement.value) &&
+        snippet.mainLibrary.contains(libraryInputElement.value) &&
         elements.any((inputElement) =>
-           snippet.centralElements.any((element) =>
+           snippet.mainElements.split(' ').any((element) =>
               element.contains(inputElement))) &&
         keywords.every((s) => snippet.description.contains(s))).toSet();
   filtered.forEach(renderSnippet);
@@ -66,17 +56,17 @@ filterGistsAndShow() {
 renderSnippet(DartSnippet snippet) {
   DivElement output = new DivElement()
     ..setInnerHtml('''
-    <b>Main library:</b> ${snippet.centralLibrary}<br>
-    <b>Main element${snippet.centralElements.length > 1 ? "s" : ""}:</b>
-    ${snippet.centralElements.toString().replaceAll('{','').replaceAll('}','')}<br>
+    <b>Main library:</b> ${snippet.mainLibrary}<br>
+    <b>Main element${snippet.mainElements.split(' ').length > 1 ? "s" : ""}:</b>
+    ${snippet.mainElements.toString().replaceAll('{','').replaceAll('}','')}<br>
     <b>Description:</b> ${snippet.description}<br>
-    <b>Author:</b> ${snippet.gist.owner.login}<br>
-    <b>Gist:</b> <a href="${snippet.gist.htmlUrl}">${snippet.gist.htmlUrl}</a><br>
-    ${snippet.centralLibrary.contains('dart') ?
+    <b>Author:</b> ${snippet.author}<br>
+    <b>Gist:</b> <a href="${snippet.gist}">${snippet.gist}</a><br>
+    ${snippet.mainLibrary.contains('dart') ?
     '''
       <b>Dartpad:</b>
-      <a href="https://dartpad.dartlang.org/${snippet.gist.id}">
-        https://dartpad.dartlang.org/${snippet.gist.id}
+      <a href="https://dartpad.dartlang.org/${snippet.id}">
+        https://dartpad.dartlang.org/${snippet.id}
       </a><br>
     ''' : ''}
     ''', validator: new TrustedNodeValidator())
