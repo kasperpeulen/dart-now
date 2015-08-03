5cfb04efb1ef645a0fc00a1357dd048a049dac4a
Kasper Peulen <kasperpeulen@gmail.com>
2015-08-03 14:49:50 +0200

    fix login issue

--- build/web/packages/petitparser/beta.dart
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains a experimental features of PetitParser. The code here
- * might be removed or changed in incompatible ways without keeping backward
- * compatibility.
- */
-library beta;
--- build/web/packages/validate/validate.dart
+++
@@ -1,4 +0,0 @@
-library validate;
-
-part "src/error/errors.dart";
-part "src/model/validate.dart";
